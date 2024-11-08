683fc8011b36bb96783e4f21c5cd97a9d5a1baf9
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-11-08 20:45:59 +0100

    Update Dashboard.tsx

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,3 +1,23 @@
+import { useFormContext } from '../store/FormContext';
+
 export default function Dashboard() {
-  return <div>Dashboard</div>;
-}
+  const { formData } = useFormContext();
+
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      {formData ? (
+        <div>
+          <p>Startup Name: {formData.startUpName}</p>
+          <p>Startup Website: {formData.startUpWebsite}</p>
+          <p>Country: {formData.selectedCountry}</p>
+          <p>Industry: {formData.selectedIndustry}</p>
+          <p>Company Tech: {formData.selectedCompanyTech}</p>
+          <p>Date Founded: {formData.dateFounded}</p>
+        </div>
+      ) : (
+        <p>No form data available.</p>
+      )}
+    </div>
+  );
+};
