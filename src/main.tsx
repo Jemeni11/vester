4893dcf16df83f0f0dc9a10d4355b01aa63cc087
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-11-08 20:41:55 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Registration, Dashboard, Error } from "./pages";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { FormProvider } from "./store/FormContext";
 import "./index.css";
 
 const router = createBrowserRouter([
@@ -19,6 +20,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <FormProvider>
+      <RouterProvider router={router} />
+    </FormProvider>
   </StrictMode>,
 );
