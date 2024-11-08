f62437ba2c84e66470f89a55d8e844dcdbe908d7
Emmanuel C. Jemeni <jemenichinonso11@gmail.com>
2024-11-08 20:39:21 +0100

    Create FormContext.tsx

---
+++ src/store/FormContext.tsx
@@ -0,0 +1,34 @@
+import React, { createContext, useContext, useState } from 'react';
+
+interface FormData {
+  startUpName: string;
+  startUpWebsite: string;
+  selectedCountry: string;
+  selectedIndustry: string;
+  selectedCompanyTech: string;
+  dateFounded: string;
+}
+
+interface FormContextProps {
+  formData: FormData | null;
+  setFormData: React.Dispatch<React.SetStateAction<FormData | null>>;
+}
+
+const FormContext = createContext<FormContextProps | undefined>(undefined);
+
+export const FormProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
+  const [formData, setFormData] = useState<FormData | null>(null);
+  return (
+    <FormContext.Provider value={{ formData, setFormData }}>
+      {children}
+    </FormContext.Provider>
+  );
+};
+
+export const useFormContext = () => {
+  const context = useContext(FormContext);
+  if (!context) {
+    throw new Error('useFormContext must be used within a FormProvider');
+  }
+  return context;
+};
